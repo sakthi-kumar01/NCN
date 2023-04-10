66dc1526b91a8eee88c52b1082a5019a005408da
raja <rampratap.sakthi22@gmail.com>
2023-04-10 18:01:02 +0530

    second commit

---
+++ NCNBackEnd/NCN BackEnd/Data/Datamanager/Contract/RemoveSubscriptionDatabaseContract.swift
@@ -0,0 +1,11 @@
+//
+//  RemoveSubscriptionDatabaseContract.swift
+//  NCN BackEnd
+//
+//  Created by raja-16327 on 10/04/23.
+//
+
+import Foundation
+public protocol RemoveSubscriptionDataContract {
+   func removeSubscription(subscriptionId: Int, success: @escaping ([String]) -> Void, failure: @escaping (String) -> Void)
+}
---
+++ NCNBackEnd/NCN BackEnd/Domain/Contract/RemoveSubscriptionDataContract.swift
@@ -0,0 +1,11 @@
+//
+//  RemoveSubscriptionDataContract.swift
+//  NCN BackEnd
+//
+//  Created by raja-16327 on 10/04/23.
+//
+
+import Foundation
+public protocol RemoveSubscriptionDatabaseContract {
+    func removeSubscription(subscriptionId: Int, success: @escaping ([String]) -> Void, failure: @escaping (String) -> Void)
+}
