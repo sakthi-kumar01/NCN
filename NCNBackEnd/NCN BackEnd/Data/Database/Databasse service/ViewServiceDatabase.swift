98a71362a3616960614b065e008b2bb50360ed5c
raja <rampratap.sakthi22@gmail.com>
2023-04-12 17:40:47 +0530

    database view service changed to get data using select query

--- NCNBackEnd/NCN BackEnd/Data/Database/Databasse service/ViewServiceDatabase.swift
+++ NCNBackEnd/NCN BackEnd/Data/Database/Databasse service/ViewServiceDatabase.swift
@@ -7,16 +7,30 @@
 
 import Foundation
 public class ViewServiceDatabase: ViewServiceDatabaseContract {
-    public init() {}
+    public init() {print("db created")}
     let db = Database.shared
-
+    var result: [AvailableService] = []
     let returnServicesOne = AvailableService(serviceId: 1, serviceTitle: "random", serviceDescription: "random")
     let returnServicesTwo = AvailableService(serviceId: 2, serviceTitle: "random", serviceDescription: "random")
     var returnService: [AvailableService] = []
-
-    public func viewService(success: @escaping ([AvailableService]) -> Void, failure _: @escaping (String) -> Void) {
-        returnService.append(returnServicesOne)
-        returnService.append(returnServicesTwo)
-        success(returnService)
+    
+    public func viewService(success: @escaping ([AvailableService]) -> Void, failure : @escaping (String) -> Void) {
+        var res = db.selectQuery(columnString: "*", tableName: "avaialableService")
+        
+        guard let resultedArray = res else {
+            failure("No  data")
+            return
+        }
+        for dict in resultedArray {
+            if let subscriptionId = dict["serviceId"] as? Int, let subscriptionPackageType = dict["serviceTitle"] as? String, let subscriptionCountLimit = dict["serviceDescription"] as? String, let serviceId = dict["subscriptionId"] {
+                var newAvailableSubscription = AvailableService(serviceId: subscriptionId, serviceTitle: subscriptionPackageType , serviceDescription: subscriptionCountLimit)
+                print(newAvailableSubscription)
+                result.append(newAvailableSubscription)
+                
+            }
+            
+            
+            success(result)
+        }
     }
 }
