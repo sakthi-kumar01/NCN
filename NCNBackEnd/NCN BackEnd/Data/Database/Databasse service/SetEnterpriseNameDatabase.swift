ba65bd9c9e3d94861d52dbf9e6263796c6d96c1b
raja <rampratap.sakthi22@gmail.com>
2023-04-13 13:42:05 +0530

    all changes

--- NCNBackEnd/NCN BackEnd/Data/Database/Databasse service/SetEnterpriseNameDatabase.swift
+++ NCNBackEnd/NCN BackEnd/Data/Database/Databasse service/SetEnterpriseNameDatabase.swift
@@ -8,7 +8,7 @@
 import Foundation
 public class SetEnterpriseDatabase: SetEnterpriseDatabaseContract {
     var db = Database.shared
-    public var enterPrise: [Enterprise] = []
+    
 
     public init() {}
 
